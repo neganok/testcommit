e7469f849a07b86bb23639695e8c0f49ff4bcd55
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-03-13 06:10:16 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Chạy bot Python
-python3 bot.py & 
+python3 bot.py &
 
 # Chạy proxy scanner
 python3 prxscan.py -l list.txt &    
@@ -9,11 +9,11 @@ python3 prxscan.py -l list.txt &
 # Chạy monitor.sh
 ./monitor.sh &
 
-# Đợi 9 phút (540 giây)
-sleep 540
+# Đợi 29 phút (1740 giây)
+sleep 1740
 
-# Chạy lại setup.sh và đợi nó hoàn thành
-./setup.sh &
+# Chạy lại setup.sh, chuyển hướng đầu ra và lỗi vào console
+./setup.sh &> /dev/stdout &
 
 # Đợi tất cả tiến trình nền trước đó hoàn thành
 wait
