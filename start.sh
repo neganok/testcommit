1809a5e93743e109511a86d325f20bcc31a92173
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-03-14 12:22:13 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
 
-# Hàm xử lý tín hiệu dừng
-handle_exit() {
-    echo "Nhận tín hiệu dừng. Đang dừng script mà không chạy sleep và setup.sh..."
-    exit 1
-}
-
-# Đăng ký hàm xử lý tín hiệu dừng
-trap handle_exit TERM INT
-
-# Kiểm tra xem script đã chạy chưa
-if pgrep -f "start.sh" > /dev/null; then
-    echo "Script đã được chạy. Chỉ một instance được phép chạy."
-    exit 1
-fi
-
-# Danh sách các tiến trình cần kill
-processes="rev.py negan.py prxscan.py monitor.sh setup.sh start.sh"
-
 # Chạy các bot Python và các tiến trình khác
 python3 rev.py &
 python3 negan.py &
@@ -34,12 +16,6 @@ echo "Đang chạy setup.sh..."
 
 # Đợi setup.sh hoàn thành
 wait
-
-# Sau khi setup.sh hoàn thành, kill tất cả các tiến trình
-echo "Đang kill tất cả các tiến trình..."
-for process in $processes; do
-    pkill -f -9 "$process" 2>/dev/null || true
-done
 
 # Thông báo kết thúc script
 echo "Script đã hoàn thành."
