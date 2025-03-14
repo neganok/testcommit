711f3db2e440d7429ef6ecd1283e2c813c3ad92b
hackerlove123 <149338972+hackerlove123@users.noreply.github.com>
2025-03-14 10:31:21 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,8 +9,8 @@ python3 prxscan.py -l list.txt &
 # Chạy monitor.sh
 ./monitor.sh &
 
-# Đợi 9 phút 30 giây (570 giây)
-sleep 570
+# Đợi 9 phút 30 giây (570 giây) trong một tiến trình riêng biệt
+(sleep 570) &
 
 # Chạy lại setup.sh, chuyển hướng đầu ra và lỗi vào console
 ./setup.sh &> /dev/stdout &
